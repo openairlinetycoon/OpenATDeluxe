a81657b2d54d5511448b268956d8531a5ed288b7
Wizzard Maker <wizzardmaker@gmail.com>
2018-01-28 20:24:24 +0100

    Added a direct loading of graphic files from a gli file.

---
+++ OpenATD/Vector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenATD.SDL
+{
+    public struct Vector
+    {
+        public float x;
+        public float y;
+
+        public Vector(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
