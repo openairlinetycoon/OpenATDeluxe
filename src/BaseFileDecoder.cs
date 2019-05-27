c0386dbf627f129017c4490276a1cbfa0238ddd5
WizzardMaker <wizzardmaker@gmail.com>
2019-05-27 23:25:49 +0200

    Added support for decrypting xtRLE files

---
+++ src/BaseFileDecoder.cs
@@ -0,0 +1,95 @@
+using Godot;
+using System;
+using BinaryWriter = System.IO.BinaryWriter;
+using MemoryStream = System.IO.MemoryStream;
+
+public class BaseFileDecoder {
+	public const string xtRLEMagic = "xtRLE";
+	private const int KEY_ONE_XTRLE = 0xa5;
+	private const int KEY_TWO_XTRLE = 0x00;
+
+	public enum DecodingMethod {
+		None,
+		xtRLE,
+	}
+
+	/// <summary>
+	/// Encrypts the data of the file according to the header of the file. 
+	/// </summary>
+	/// <param name="fileIn">An already opened file</param>
+	/// <returns>Returns the decrypted file ready to be filled in a document.</returns>
+	public byte[] ReadFile(File fileIn) {
+		DecodingMethod method = IsXTRLE(fileIn);
+
+		byte[] data = null;
+
+		switch (method) {
+			case (DecodingMethod.None):
+
+				break;
+			case (DecodingMethod.xtRLE):
+				data = DecodeXTRLE(fileIn);
+				break;
+		}
+
+		return data;
+	}
+
+	/// <summary>
+	/// Checks the header of the file for xtRLE
+	/// </summary>
+	/// <param name="fileIn">An already opened file</param>
+	/// <returns>If file is an xtRLE file: DecodingMethod.xtRLE; if not DecodingMethod.None</returns>
+	private static DecodingMethod IsXTRLE(File fileIn) {
+		byte[] magic = fileIn.GetBuffer(xtRLEMagic.Length);
+		DecodingMethod method = xtRLEMagic.ToAscii() == magic ? DecodingMethod.xtRLE : DecodingMethod.None;
+		fileIn.Seek(0); //Go back to the start of the file
+		return method;
+	}
+
+	//
+	/// <summary>
+	/// Decodes an xtRLE file
+	/// Checks the header of the file for xtRLE
+	/// </summary>
+	/// <param name="fileIn">An already opened file</param>
+	/// <returns>Returns the decrypted xtRLE file in a byte[]</returns>
+	private byte[] DecodeXTRLE(File fileIn) {
+		byte[] data;
+
+		fileIn.Seek(10); //Skip header
+		int finalSize = fileIn.Get32(); //Get file size
+
+		data = null;
+
+		using (MemoryStream ms = new MemoryStream())
+		using (BinaryWriter bw = new BinaryWriter(ms)) { //Easy way to write the file into a byte[]
+			uint num;//How far we should skip / how many times we should repeat the byte
+
+			while (fileIn.GetError() != Error.FileEof) {
+				byte command = (byte)fileIn.Get8(); //Get the next command
+				num = (uint)(command & 0b0111_1111);//0b0111_1111;
+
+				if ((command & 0b1000_0000) == 0b1000_0000) { //Copy data command
+					byte[] bytes = fileIn.GetBuffer(checked((int)num)); //Get the data
+					for (int i = 0; i < bytes.Length; i++) {
+						bytes[i] ^= KEY_ONE_XTRLE; //Decrypt it with KEY_ONE
+					}
+
+					bw.Write(bytes); //Save it to the stream
+				} else if ((command & 0b1000_0000) == 0b0000_0000) { //Repeat the following bytes "num" times
+					byte repeatingData = (byte)fileIn.Get8(); //What Byte to repeat
+
+					repeatingData ^= KEY_TWO_XTRLE; //Decrypt it with KEY_TWO
+
+					for (int i = 0; i < num; i++) //Repeat and save
+						bw.Write(repeatingData);
+				}
+			}
+
+			data = ms.ToArray(); //Save to array
+		}
+
+		return data;
+	}
+}
