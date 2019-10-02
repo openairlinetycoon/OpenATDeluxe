c1ed4e2c15fadbeb4b6d392f780fcd0fd43d3564
WizzardMaker <wizzardmaker@gmail.com>
2019-10-02 20:55:45 +0200

    Ability to create MouseAreas in Code

--- src/MouseArea.cs
+++ src/MouseArea.cs
@@ -5,7 +5,19 @@ public class MouseArea : Area2D {
 	[Export]
 	public bool isExitToAirport = false;
 
+	public Action onClick;
+	public CollisionShape2D area;
+
 	public override void _Ready() {
+		if (GetChildCount() != 0)
+			area = (CollisionShape2D)GetChild(0);
+
+		if (area == null) {
+			area = new CollisionShape2D();
+			area.Shape = new RectangleShape2D();
+			AddChild(area);
+		}
+
 		Connect("mouse_entered", this, nameof(MouseEntered));
 		Connect("mouse_exited", this, nameof(MouseExited));
 	}
@@ -19,6 +31,6 @@ public class MouseArea : Area2D {
 
 
 	public virtual void OnClick() {
-
+		onClick?.Invoke();
 	}
 }
