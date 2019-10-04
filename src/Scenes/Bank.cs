de966fb0adef0c9c9155c3cc6c83e4762803362e
WizzardMaker <wizzardmaker@gmail.com>
2019-10-05 01:36:40 +0200

    More juicy test implementation
    and WAAAY MORE crashes, because of the godot bug :angry:
    Also moved the bank interiour a bit more into frame

--- src/Scenes/Bank.cs
+++ src/Scenes/Bank.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class Bank : Node2D {
 	public Control baseNode;
 
-	AnimationList creditAnims = new AnimationList();
+	AnimationList loanAnims = new AnimationList();
 	AnimationList sharesAnims = new AnimationList();
 	public override void _Ready() {
 		AddToGroup("cancelable");
@@ -14,19 +14,19 @@ public class Bank : Node2D {
 
 		Vector2 playerPos = new Vector2(-280, -90);
 
-		creditAnims.basePosition = new Vector2(-142, -73);
+		loanAnims.basePosition = new Vector2(-142, -73);
 		sharesAnims.basePosition = new Vector2(33, -63);
-		creditAnims.CreateMouseArea(baseNode);
+		loanAnims.CreateMouseArea(baseNode);
 		sharesAnims.CreateMouseArea(baseNode);
 
 
-		Dialogue creditDialogue = new Dialogue("Bank");
+		Dialogue loanDialogue = new Dialogue("Bank");
 		{
 			DialogueNode startCredit = new DialogueNode(109);
 			DialogueNodeReturning noNewLoan = new DialogueNodeReturning(120);
 			DialogueNodeReturning noReturningOfLoan = new DialogueNodeReturning(140);
 
-			creditDialogue.AddNode(startCredit)
+			loanDialogue.AddNode(startCredit)
 				.AddNode(noNewLoan)
 				.AddNode(noReturningOfLoan);
 
@@ -35,29 +35,53 @@ public class Bank : Node2D {
 			startCredit.AddOption(new DialogueOptionReturning(103));
 		}
 
-		Dialogue shareDialogue = new Dialogue("Bank");
+		Dialogue sharesDialogue = new Dialogue("Bank");
 		{//keep them in scope, so that we don't use the wrong nodes from the prev. dialogue
 			DialogueNode startShare = new DialogueNode(900);
+
+			DialogueNode buyShares = new DialogueNode(2000) { returnable = false };
 			DialogueNodeReturning noShares = new DialogueNodeReturning(1200);
 			DialogueNodeReturning noSelling = new DialogueNodeReturning(1100);
 
-			shareDialogue.AddNode(startShare)
-				.AddNode(noShares)
-				.AddNode(noSelling);
-
-			startShare.AddOption(new DialogueOption(1000, noShares));
+			startShare.AddOption(new DialogueOption(1000, buyShares));
 			startShare.AddOption(new DialogueOption(1001, noSelling));
 			startShare.AddOption(new DialogueOptionReturning(1005));
+
+			//buyShares 2000
+			DialogueNode okBuyAnother = new DialogueNode(2030) { returnable = false };
+
+			buyShares.AddOption(new DialogueOption(2010, okBuyAnother, GameController.playerCompanyNames[0], GameController.playerNames[0]));
+			buyShares.AddOption(new DialogueOption(2011, okBuyAnother, GameController.playerCompanyNames[1], GameController.playerNames[1]));
+			buyShares.AddOption(new DialogueOption(2012, okBuyAnother, GameController.playerCompanyNames[2], GameController.playerNames[2]));
+			buyShares.AddOption(new DialogueOption(2013, okBuyAnother, GameController.playerCompanyNames[3], GameController.playerNames[3]));
+			buyShares.AddOption(new DialogueOptionReturning(2014));
+
+			//okBuyAnother
+
+			okBuyAnother.AddOption(new DialogueOption(2020, okBuyAnother, GameController.playerCompanyNames[0], GameController.playerNames[0]));
+			okBuyAnother.AddOption(new DialogueOption(2021, okBuyAnother, GameController.playerCompanyNames[1], GameController.playerNames[1]));
+			okBuyAnother.AddOption(new DialogueOption(2022, okBuyAnother, GameController.playerCompanyNames[2], GameController.playerNames[2]));
+			okBuyAnother.AddOption(new DialogueOption(2023, okBuyAnother, GameController.playerCompanyNames[3], GameController.playerNames[3]));
+			okBuyAnother.AddOption(new DialogueOptionReturning(2024));
+
+
+
+			sharesDialogue.AddNode(startShare);
+			// 	.AddNode(buyShares)
+			// 	.AddNode(okBuyAnother)
+			// 	.AddNode(noShares)
+			// 	.AddNode(noSelling);
+
 		}
-		creditAnims.mouseArea.onClick += () => DialogueSystem.StartDialogue(creditDialogue, "P2", "B2");
-		sharesAnims.mouseArea.onClick += () => DialogueSystem.StartDialogue(shareDialogue, "P2", "BA");
+		loanAnims.mouseArea.onClick += () => DialogueSystem.StartDialogue(loanDialogue, "P2", "B2");
+		sharesAnims.mouseArea.onClick += () => DialogueSystem.StartDialogue(sharesDialogue, "P2", "BA");
 		//DialogueSystem.StartDialogue(shareDialogue, "P2", "BA");
 		string player = "PL2";
 
 		bool alreadyFired = false;
-		bool TriggerIdleAnimationCredit(AnimationGoal goal) {
+		bool TriggerIdleAnimation(AnimationGoal goal, Dialogue thisDialogue) {
 			//If dialogue is running:
-			if (DialogueSystem.currentDialogue == creditDialogue) {
+			if (DialogueSystem.currentDialogue == thisDialogue) {
 				goal.triggerID = 2;
 				return true;
 			}
@@ -68,59 +92,28 @@ public class Bank : Node2D {
 			goal.triggerID = 1;
 			return ret;
 		}
-		bool TriggerIdleAnimationShare(AnimationGoal goal) {
-			//If dialogue is running:
-			if (DialogueSystem.currentDialogue == shareDialogue) {
-				goal.triggerID = 2;
-				return true;
-			}
-
-			bool ret = !alreadyFired;
-			alreadyFired = true;
-
-			goal.triggerID = 1;
-			return ret;
-		}
-		// bool WhileListening(AnimationGoal goal) {
-		// 	if (DialogueSystem.currentlyTalking == "B2") {
-		// 		goal.triggerID = 6;
-		// 		return true;
-		// 	}
-		// 	if (DialogueSystem.currentDialogue != creditDialogue) {
-		// 		goal.triggerID = 3;
-		// 		return true;
-		// 	}
-
-		// 	return WhileTalkingAnimHelper(g, creditDialogue, "B2", 6, 3);
-		// }
-		// bool WhileTalking(AnimationGoal goal) {
-		// 	const string actor = "B2";
-		// 	const int noTalkingID = 4;
-		// 	const int dialogueStoppedID = 3;
-		// 	return WhileTalkingAnimHelper(goal, creditDialogue, actor, noTalkingID, dialogueStoppedID);
-		// }
 
 
-		creditAnims.Add(
-			SmkAnimation.CreateAnimation(baseNode, "BF_Wait.smk", goals: new AnimationGoal(onTrigger: TriggerIdleAnimationCredit)));
-		creditAnims.Add(
+		loanAnims.Add(
+			SmkAnimation.CreateAnimation(baseNode, "BF_Wait.smk", goals: new AnimationGoal(onTrigger: (g) => TriggerIdleAnimation(g, loanDialogue))));
+		loanAnims.Add(
 			SmkAnimation.CreateAnimation(baseNode, "BF_Work.smk", goals: new AnimationGoal(finish: 0, cancel: 0)));
-		creditAnims.Add(
+		loanAnims.Add(
 			SmkAnimation.CreateAnimation(baseNode, "BF_Turn.smk", goals: new AnimationGoal(finish: 4, cancel: 4)));
-		creditAnims.Add(
+		loanAnims.Add(
 			SmkAnimation.CreateAnimation(baseNode, "BF_TurnZ.smk", goals: new AnimationGoal(finish: 0)));
-		creditAnims.Add(
+		loanAnims.Add(
 			SmkAnimation.CreateAnimation(baseNode, "BF_RedeW.smk",
-			goals: new AnimationGoal(onTrigger: (g) => WhileListeningAnimHelper(g, creditDialogue, "B2", 6, 3))));
-		creditAnims.Add(
+			goals: new AnimationGoal(onTrigger: (g) => WhileListeningAnimHelper(g, loanDialogue, "B2", 6, 3))));
+		loanAnims.Add(
 			SmkAnimation.CreateAnimation(baseNode, "BF_RedeK.smk",
-			goals: new AnimationGoal(finish: 4, onTrigger: (g) => WhileListeningAnimHelper(g, creditDialogue, "B2", 6, 3))));
-		creditAnims.Add(
+			goals: new AnimationGoal(finish: 4, onTrigger: (g) => WhileListeningAnimHelper(g, loanDialogue, "B2", 6, 3))));
+		loanAnims.Add(
 			SmkAnimation.CreateAnimation(baseNode, "BF_Rede.smk",
-			goals: new AnimationGoal(onTrigger: (g) => WhileTalkingAnimHelper(g, creditDialogue, "B2", 4, 3))));
+			goals: new AnimationGoal(onTrigger: (g) => WhileTalkingAnimHelper(g, loanDialogue, "B2", 4, 3))));
 
 		sharesAnims.Add(
-			SmkAnimation.CreateAnimation(baseNode, "BM_Wait.smk", goals: new AnimationGoal(onTrigger: TriggerIdleAnimationShare)));
+			SmkAnimation.CreateAnimation(baseNode, "BM_Wait.smk", goals: new AnimationGoal(onTrigger: (g) => TriggerIdleAnimation(g, sharesDialogue))));
 		sharesAnims.Add(
 			SmkAnimation.CreateAnimation(baseNode, "BM_Scrib.smk", goals: new AnimationGoal(finish: 0, cancel: 0)));
 		sharesAnims.Add(
@@ -129,16 +122,16 @@ public class Bank : Node2D {
 			SmkAnimation.CreateAnimation(baseNode, "BM_TurnZ.smk", goals: new AnimationGoal(finish: 0)));
 		sharesAnims.Add(
 			SmkAnimation.CreateAnimation(baseNode, "BM_RedeW.smk",
-			goals: new AnimationGoal(onTrigger: (g) => WhileListeningAnimHelper(g, shareDialogue, "BA", 6, 3))));
+			goals: new AnimationGoal(onTrigger: (g) => WhileListeningAnimHelper(g, sharesDialogue, "BA", 6, 3))));
 		sharesAnims.Add(
 			SmkAnimation.CreateAnimation(baseNode, "BM_RedeK.smk",
-			goals: new AnimationGoal(finish: 4, onTrigger: (g) => WhileListeningAnimHelper(g, shareDialogue, "BA", 6, 3))));
+			goals: new AnimationGoal(finish: 4, onTrigger: (g) => WhileListeningAnimHelper(g, sharesDialogue, "BA", 6, 3))));
 		sharesAnims.Add(
 			SmkAnimation.CreateAnimation(baseNode, "BM_Rede.smk",
-			goals: new AnimationGoal(onTrigger: (g) => WhileTalkingAnimHelper(g, shareDialogue, "BA", 4, 3))));
+			goals: new AnimationGoal(onTrigger: (g) => WhileTalkingAnimHelper(g, sharesDialogue, "BA", 4, 3))));
 
 
-		creditAnims.Play(0);
+		loanAnims.Play(0);
 		sharesAnims.Play(0);
 		GetTree().CallGroup("cancelable", "Cancel");
 	}
@@ -169,7 +162,7 @@ public class Bank : Node2D {
 	}
 
 	override public void _Process(float delta) {
-		creditAnims.ProcessTrigger();
+		loanAnims.ProcessTrigger();
 		sharesAnims.ProcessTrigger();
 	}
 
